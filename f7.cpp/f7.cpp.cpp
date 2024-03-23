8e65191c1f8e37c131ec3c2ee8c3fcc2e03e38bc
igpraise <igpraise45@gmail.com>
2024-03-22 22:27:50 -0400

    adding binary file

--- f7.cpp/f7.cpp.cpp
+++ f7.cpp/f7.cpp.cpp
@@ -6,7 +6,6 @@
 * DESCRIPTION :
 * The functions in this file are used to write this program in C onlyWrite a program to give you practice at creating text and binary files 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,25 +17,18 @@ void checkError(FILE* file, const char* message) {
     }
 }
 
-// 
+// Function to write text file
+void writeTextFile() {
+    FILE* file = fopen("myTextFile.txt", "w");
+    checkError(file, "Error opening text file for writing");
 
-// Function to write binary file
-void writeBinaryFile() {
-    FILE* file = fopen("myEvenDataList.data", "wb");
-    checkError(file, "Error opening binary file for writing");
-
-    const unsigned short kBinaryData[] = { 26946, 24942, 31090,
-                                            25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
-    size_t numElements = sizeof(kBinaryData) / sizeof(kBinaryData[0]);
-
-    for (size_t i = 0; i < numElements; i++) {
-        if (kBinaryData[i] % 2 == 0) {
-            fwrite(&kBinaryData[i], sizeof(kBinaryData[i]), 1, file);
-        }
-    }
+    fprintf(file, "This is line 1.\n");
+    fprintf(file, "This is line 2.\n");
 
     fclose(file);
 }
+
+
 
 int main() {
     writeTextFile();
